5cc724c00f8e9d45ad567cfba70d2bf0489535b9
Sahil Singh <114878612+sahilsingh12221802@users.noreply.github.com>
2024-01-31 13:43:50 +0530

    Update commit

--- Advance_Data_Types_Lec-5_Vectors.R
+++ Advance_Data_Types_Lec-5_Vectors.R
@@ -111,6 +111,114 @@ which(y == max(y))
 which(y == min(y)) # (It gives the index of duplicate elements)
 
 
+data1 <- c(3,5,7,5,3,2,6,8,5,6,9)
+data1
+length(data1)
+data1==6 # Extracting location of the element 6(It will give true and false)
+which(data1==6) # It will give the index
+
+data1 > 6
+which(data1 > 6)
+data1[data1>7]
+
+
+y <- c(8,6,7,5,7)
+y[3]
+
+# Assigning NULL value to this y vector
+y <- NULL
+y
+y[3]
+
+
+
+y <- 12 : 17
+y
+length(y)
+
+
+# Add 18,19,20 in y vector
+y1 <- c(y, 18,19,20)
+y
+y1 # Added 3 new values to the vector
+length(y1)
+
+
+y <- 6.6 : 12.6 # Sequence of numbers using decimal lower and upper bounds
+y
+
+y <- 6.6 : 12.5 # Print till 11.6 
+y
+
+y <- 3.8:11.4 # Print till 10.8
+y
+
+
+# 3rd Method to create vectors - seq()
+seq(1:6)
+
+sq <- seq(1,6) # we can use comma or colon
+sq
+
+sq <- seq(1,6, by = .2) # for gap between numbers(changing the separator)
+sq
+
+sq <- seq(1,6, by = 2) # printing odd numbers
+sq
+
+sq <- seq(0,10, by = 2) # printing even numbers
+sq
+
+# Separator is decided by the length.out
+sq <- seq(1,6, length.out = 3) # we are changing the length from 6 to 3
+sq
+
+sq <- seq(1,10, length.out = 3) # we are changing the length from 10 to 3
+sq
+
+sq <- seq(1,10, length.out = 5) # we are changing the length from 10 to 5
+sq
+
+sq <- seq(1,10, length.out = 3)
+sq
+sq <- seq(1,20, length.out = 5)
+sq
+
+
+# Different Vectors
+
+# numeric vectors
+a <- c(1,2,3,4,5)
+a
+# character vectors
+b <- c('hi', 'hello', 'bye','wow','nice')
+b
+# integer vectors
+c <- c(1L,2L,3L,4L,5L)
+c
+# logical vectors
+d <- c(T,T,F,T,F)
+d
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
